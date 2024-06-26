3ecc9445bd66268b5403f53cd35ab1747f2f5a7f
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-06-27 00:21:46 +0500

    Miles to Kilometers

--- Mile-to-Kilometer-in-Python.py
+++ Mile-to-Kilometer-in-Python.py
@@ -1,1 +1,35 @@
-print("Hello World")
+from tkinter import *
+
+def milesToKm():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    kilometer_result_label.config(text=f"{km}")
+
+
+window = Tk()
+window.title("Miles to Kilometers Converter")
+
+miles_input = Entry()
+miles_input.grid(column=1, row =0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row =0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row =1)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row =1)
+
+kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row =1)
+
+calculate_button = Button(text="Calculate", command=milesToKm)
+calculate_button.grid(column=1, row =2)
+
+
+
+
+
+
+window.mainloop()
